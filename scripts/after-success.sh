565a3157423205773e7689237666f472d4a5ab30
Matias Moeskjær Gramkow <mati2441@stud.kea.dk>
2021-07-19 17:29:13 +0200

    new scripts

--- scripts/after-success.sh
+++ scripts/after-success.sh
@@ -1,1 +1,5 @@
 docker build -t matiasgramkow23/dockervueclient ./vuedocker
+docker build -t matiasgramkow23/dockerapi ./dockerapi
+docker build -t matiasgramkow23/dockernginx ./nginx
+docker build -t matiasgramkow23/dockerunittestdotnet ./unittestdotnet
+docker build -t matiasgramkow23/dockerwebapi ./webapi
